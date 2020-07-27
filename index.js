6c4b310df6aaf9534b74068874a3ecb1a4aee5ae
Jakub Sikora <j.sikora@livechatinc.com>
2020-07-27 13:51:25 +0200

    new model

--- index.js
+++
@@ -1,7 +0,0 @@
-window.onMomentAsyncInit = function (createMomentsSDK) {
-  createMomentsSDK({ title: "My App" }).then((momentsSDK) => {
-    console.log(window.MomentsSDK);
-    //window.MomentsSDK.sendMessage({ text: "yes" });
-    // your code
-  });
-};
